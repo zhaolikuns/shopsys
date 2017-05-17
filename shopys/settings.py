6b2548824e1c876ad770ce0112bb8e1e9240032a
zhaolikun <355149444@qq.com>
2017-05-17 16:37:23 +0800

    更改数据库为mysql

--- shopys/settings.py
+++ shopys/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'shopys.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'shopsys',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'USER': 'zhaolikun',
+        'PASSWORD': '25498887zlk'
     }
 }
 
